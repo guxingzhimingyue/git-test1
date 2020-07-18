c8d4379370cd33ebafbab868e795315f2ffd36c6
admin <admin@qq.com>
2020-07-18 23:37:01 +0800

    这是刚开始的测试方法

---
+++ test-1/src/cn/test/Test1.java
@@ -0,0 +1,11 @@
+package cn.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("这是一个新的方法");
+        System.out.println("这是一个新的方法");
+        System.out.println("这是一个新的方法");
+        System.out.println("这是一个新的方法");
+
+    }
+}
